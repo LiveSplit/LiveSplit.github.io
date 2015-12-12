d10f710a487cf4aeccec543be2a2f68557b8c9fe
Sergey Papushin <spapushin@gmail.com>
2015-12-12 13:35:02 -0600

    Make Travis CI upload to FTP

--- cibuild.sh
+++ cibuild.sh
@@ -3,3 +3,9 @@ set -e # halt script on error
 
 bundle exec jekyll build
 bundle exec htmlproof ./_site
+
+if [ "$TRAVIS_BRANCH" = "master" ]; then
+	echo "Starting to upload..."
+	lftp -c "open -u livesplit,$FTP_PASS ftp://livesplit.org; mirror -R -i .*$ '_site/' ."
+	echo "Upload finished."
+fi
