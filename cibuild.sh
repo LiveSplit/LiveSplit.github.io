444f8173b3ad972b9ee667a0ce38a9477db8eb5e
Sergey Papushin <spapushin@gmail.com>
2018-10-05 16:44:18 -0500

    Ignore URLs from bootstrap

--- cibuild.sh
+++ cibuild.sh
@@ -2,7 +2,7 @@
 set -e # halt script on error
 
 bundle exec jekyll build
-bundle exec htmlproofer ./_site
+bundle exec htmlproofer --url-ignore "/maxcdn.bootstrapcdn.com/" ./_site
 
 if [ "$TRAVIS_BRANCH" = "master" ]; then
 	echo "Starting to upload..."
