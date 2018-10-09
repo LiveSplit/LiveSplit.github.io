58c6a1c35fba36abd2b176bc48eb8a4b961af3a5
Sergey Papushin <spapushin@gmail.com>
2018-10-09 02:51:54 -0500

    Get rid of uploading to livesplit.org

--- cibuild.sh
+++ cibuild.sh
@@ -3,10 +3,3 @@ set -e # halt script on error
 
 bundle exec jekyll build
 bundle exec htmlproofer --url-ignore "/maxcdn.bootstrapcdn.com/" ./_site
-
-if [ "$TRAVIS_BRANCH" = "master" ]; then
-	echo "Starting to upload..."
-	cd _site
-	find . -type f -exec curl -u livesplit@livesplit.org:$FTP_PASS --ftp-create-dirs -T {} ftp://livesplit.org/{} \;
-	echo "Upload finished."
-fi
