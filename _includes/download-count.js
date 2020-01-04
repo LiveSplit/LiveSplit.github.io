8f124e05b9ea10aa97fdb170f412f525c078aed5
wooferzfg <spapushin@gmail.com>
2020-01-03 18:19:37 -0600

    Update download-count.js to make the request asynchronously

--- _includes/download-count.js
+++ _includes/download-count.js
@@ -1,13 +1,18 @@
 var xmlHttp = new XMLHttpRequest();
-xmlHttp.open("GET", "https://api.github.com/repos/LiveSplit/LiveSplit/releases", false);
-xmlHttp.send(null);
 
-var releasesData = JSON.parse(xmlHttp.responseText);
-var totalDownloads = releasesData.reduce(
-  function (total, current) {
-    return total + current.assets[0].download_count;
-  },
-  145579 // pre-GitHub download total
-);
+xmlHttp.onreadystatechange = function() {
+  if (xmlHttp.readyState === 4) {
+    var releasesData = JSON.parse(xmlHttp.responseText);
+    var totalDownloads = releasesData.reduce(
+      function (total, current) {
+        return total + current.assets[0].download_count;
+      },
+      145579 // pre-GitHub download total
+    );
+    
+    document.getElementById("download-count").innerText = totalDownloads + " downloads";
+  }
+};
 
-document.getElementById("download-count").innerText = totalDownloads + " downloads";
+xmlHttp.open("GET", "https://api.github.com/repos/LiveSplit/LiveSplit/releases");
+xmlHttp.send();
