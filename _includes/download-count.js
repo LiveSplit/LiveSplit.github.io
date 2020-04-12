7f57375c6a1b5534f3721529ca14889707bb8301
wooferzfg <spapushin@gmail.com>
2020-04-12 13:05:45 -0500

    Fix download count not including more than 30 releases

--- _includes/download-count.js
+++ _includes/download-count.js
@@ -1,18 +1,31 @@
-var xmlHttp = new XMLHttpRequest();
 
-xmlHttp.onreadystatechange = function() {
+
+var totalDownloads = 145579; // pre-GitHub download total
+
+var handleApiResponse = function() {
   if (xmlHttp.readyState === 4) {
     var releasesData = JSON.parse(xmlHttp.responseText);
-    var totalDownloads = releasesData.reduce(
+    totalDownloads += releasesData.reduce(
       function (total, current) {
         return total + current.assets[0].download_count;
       },
-      145579 // pre-GitHub download total
+      0
     );
-    
-    document.getElementById("download-count").innerText = totalDownloads + " downloads";
+
+    var linkHeader = xmlHttp.getResponseHeader("Link");
+    var nextUrlMatch = linkHeader.match(/<(.+)>; rel="next"/);
+    if (nextUrlMatch) {
+      xmlHttp = new XMLHttpRequest();
+      xmlHttp.onreadystatechange = handleApiResponse;
+      xmlHttp.open("GET", nextUrlMatch[1]);
+      xmlHttp.send();
+    } else {
+      document.getElementById("download-count").innerText = totalDownloads + " downloads";
+    }
   }
 };
 
+var xmlHttp = new XMLHttpRequest();
+xmlHttp.onreadystatechange = handleApiResponse;
 xmlHttp.open("GET", "https://api.github.com/repos/LiveSplit/LiveSplit/releases");
 xmlHttp.send();
